698417173252d5d929bca38942c9cff1b52cacd3
papp <papp.csenge0@gmail.com>
2022-09-16 16:15:57 +0200

    feat:create empty flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask('movie-finder')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def main():
+    app.run(debug=False)
+
+
+if __name__ == '__main__':
+    main()
+    
