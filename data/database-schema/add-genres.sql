416e9766e1a417ff71a1ee54a908a7250d42177c
papp <papp.csenge0@gmail.com>
2022-09-17 21:55:45 +0200

    feat: fill database with genres

---
+++ data/database-schema/add-genres.sql
@@ -0,0 +1,13 @@
+/* genres are: sci-fi, horror, humor and adventure */
+
+INSERT INTO genres (id, name)
+    VALUES (0,'Sci-fi');
+
+INSERT INTO genres (id, name)
+    VALUES (1,'Adventure');
+
+INSERT INTO genres (id, name)
+    VALUES (2,'Horror');
+
+INSERT INTO genres (id, name)
+    VALUES (3,'Humor');
---
+++ data/database-schema/add-movie-genres.sql
@@ -0,0 +1,34 @@
+/* connecting movies with their genre */
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (0,0,0);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (1,1,0);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (2,2,0);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (3,3,0);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (4,4,1);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (5,5,1);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (6,6,1);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (7,7,2);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (8,8,2);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (9,9,2);
+
+INSERT INTO movie_genres (id, movie_id, genre_id)
+    VALUES (10,10,3);
